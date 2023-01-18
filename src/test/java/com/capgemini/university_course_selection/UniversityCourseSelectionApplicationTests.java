1c17ef72d7718ef3ae6ec81f8d322f6d366c7528
Sindhu-BB <sindhubaleholada@gmail.com>
2023-01-18 10:02:29 +0530

    added

---
+++ src/test/java/com/capgemini/university_course_selection/UniversityCourseSelectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.university_course_selection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityCourseSelectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
